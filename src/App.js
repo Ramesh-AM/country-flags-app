3b1202bec6ba38343649490cd65521454fe72533
ramesham3818 <ramesham3818@gmail.com>
2025-08-01 23:24:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const API = 'https://xcountries-backend.azurewebsites.net/all';
+  useEffect(() => {
+    fetch(API)
+      .then((res) =>res.json())
+      .then((jsonRes) => setData(jsonRes))
+      .catch ((error) => console.error('Error fetching data:', error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: "flex",  flexWrap: "wrap", gap: "10px"}}>
+      {data.map((item, index) => (
+        <div key={index} className="item" style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "4px", border: "1px solid black", borderRadius: "4px", width: "200px", height: "200px"}}>
+          {console.log('Rendering item:', item.image)}
+          <img src='' alt={`Flag of ${item.abbr}`}/>
+          <h2>{item.name}</h2>
+        </div> 
+      ))}
     </div>
   );
 }
